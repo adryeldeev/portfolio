740034f4308142480019df1f5aafe12bdb7682a1
adryeldeev <adryeldev@hotmail.com>
2025-11-03 14:46:39 -0300

    adicionando projeto

--- src/Components/Projetos/Projetos.tsx
+++ src/Components/Projetos/Projetos.tsx
@@ -2,6 +2,7 @@ import fourseguro from '../../assets/4slp.png'; // Certifique-se de que este cam
 import lpfc from '../../assets/LPFC.png';     // Certifique-se de que este caminho está correto
 import sistema from '../../assets/4sSitema.png'; // Certifique-se de que este caminho está correto
 import advogadoTrabalhista from '../../assets/lp-advogadoTrabalhista.png'
+import jpTecidos from '../../assets/jptecidos.png'
 const Projetos = () => {
   return (
     <section id="projetos" className="py-24 px-6 bg-[#0D1117] text-white">
@@ -151,6 +152,46 @@ const Projetos = () => {
              <div className="flex gap-4">
                 <a
                   href="https://advogado-paulomendes.netlify.app/"
+                  className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg font-semibold text-sm"
+                >
+                  View Project
+                </a>
+              
+              
+              </div>
+             
+            </div>
+          </div>
+            <div className="bg-[#111827] rounded-xl shadow-lg overflow-hidden border border-[#1E293B] flex flex-col">
+            {/* Imagem no topo do card */}
+            <div className="w-full h-48 sm:h-56 md:h-64 flex items-center justify-center p-4">
+              <img
+                src={jpTecidos}
+
+                alt="Management System - 4S Seguros"
+                className="w-full h-full object-contain rounded-lg border border-[#374151]"
+              />
+            </div>
+
+            {/* Texto abaixo da imagem */}
+            <div className="flex-1 p-6 flex flex-col">
+              <h3 className="text-2xl font-bold text-white mb-2">Landing Page - JP Tecidos</h3>
+
+              <p className="text-blue-400 font-medium mb-2">Soluções em tecidos</p>
+
+              <p className="text-gray-400 mb-4 text-sm">
+                A Landing Page da JP Tecidos foi desenvolvida para apresentar os produtos têxteis de forma clara e atraente. O design responsivo garante uma ótima experiência em dispositivos móveis e desktops.
+              </p>
+
+              {/* Tags */}
+              <div className="flex gap-2 flex-wrap mb-6 mt-auto">
+                <span className="bg-gray-700 text-xs px-2 py-1 rounded-full text-white">React</span>
+                <span className="bg-gray-700 text-xs px-2 py-1 rounded-full text-white">Tailwind</span>
+              </div>
+
+             <div className="flex gap-4">
+                <a
+                  href="https://lp-lojajptecidos.vercel.app/"
                   className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg font-semibold text-sm"
                 >
                   View Project
